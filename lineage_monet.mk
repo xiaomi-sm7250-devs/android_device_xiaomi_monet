a681102b3a4b1671ae1ce5ce75fb00b442310fb4
xSylla <syllamodder@gmail.com>
2022-05-03 23:42:21 +0200

    monet: Update blobs from MIUI V13.0.2.0.SJIMIXM
    Change-Id: I278e93f784418073286600df60937680f491c56c

--- lineage_monet.mk
+++ lineage_monet.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := M2002J9G
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="monet-user 11 RKQ1.200826.002 V12.5.5.0.RJIEUXM release-keys"
+    PRIVATE_BUILD_DESC="monet-user 12 SKQ1.211006.001 V13.0.2.0.SJIEUXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/monet_eea/monet:11/RKQ1.200826.002/V12.5.5.0.RJIEUXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/monet_eea/monet:12/SKQ1.211006.001/V13.0.2.0.SJIEUXM:user/release-keys
