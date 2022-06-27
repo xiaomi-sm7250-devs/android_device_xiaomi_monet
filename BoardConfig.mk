85492cffe1914241a086572c7103de7307110bc0
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-27 02:22:05 +0200

    monet: Add libinit for variants handling
    Change-Id: Idff34bdf8270270bfa60b081b4e631dce6436a11

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,9 @@ BUILD_BROKEN_DUP_RULES := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_monet
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_monet
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/monet_user_defconfig
 
---
+++ libinit/init_xiaomi_monet.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t monet_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Xiaomi",
+    .device = "monet",
+    .marketname = "Mi 10 Lite 5G",
+    .model = "M2002J9G",
+    .build_fingerprint = "Xiaomi/monet_eea/monet:12/SKQ1.211006.001/V13.0.2.0.SJIEUXM:user/release-keys",
+
+    .nfc = true,
+};
+
+static const std::vector<variant_info_t> variants = {
+    monet_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+}
